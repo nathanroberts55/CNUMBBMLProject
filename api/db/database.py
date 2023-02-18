74edf7c1dd54f1476a33e5b2cf1de8f2d6860dbb
nathanroberts55 <nathan.roberts.15@cnu.edu>
2023-02-18 16:54:31 -0500

    Updated database name to fit gitignore

--- api/db/database.py
+++ api/db/database.py
@@ -1,7 +1,7 @@
 from sqlmodel import SQLModel, create_engine
 from .models import *
 # UIsing SQLite here but can easily use PostgreSQL by changing the url
-sqlite_file_name = "stats.sqlite3"
+sqlite_file_name = "db.sqlite3"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 connect_args = {"check_same_thread": False}
 
